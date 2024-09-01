c55aee5d1cfa3795bab25cadce897183a04dc97d
Nikhil Gupta <108759735+nick5776@users.noreply.github.com>
2024-09-01 23:26:58 +0530

    Add files via upload

---
+++ backend/src/middlewares/fetchuser.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const fetchuser = (req,res,next)=>{
+    const authToken = req.header('auth-token');
+
+    if(!authToken){
+        res.status(401).send({error:"Invalid auth token"});
+    }
+
+    try{
+        console.log(authToken);
+        const data = jwt.verify(authToken,JWT_SECRET_KEY);
+        req.user = data.user;
+        console.log("dfddd",req.user);
+        next();
+    }catch(err){
+        res.status(401).send({error:"Internal server error"});
+    }
+};
+
+module.exports = fetchuser;
